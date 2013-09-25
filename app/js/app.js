6c991b15b4980e0c2739dc948b025ae6261caf2e
holgergp <holgergp@gmail.com>
2013-09-25 19:55:13 +0200

    Angular.js läuft

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,10 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+angular.module('halbtonApp', ['halbtonApp.filters', 'halbtonApp.services', 'halbtonApp.directives', 'halbtonApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    /*$routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});*/
+    $routeProvider.when('/default', {templateUrl: 'partials/halbtonberechnung.html', controller: 'HalbtonController'});
+    $routeProvider.otherwise({redirectTo: '/default'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,55 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
 
-  }])
-  .controller('MyCtrl2', [function() {
+var halbtonApp = angular.module('halbtonApp.controllers', []);
+halbtonApp.controller('HalbtonController', ['$scope', function ($scope) {
+  $scope.halbtoene = [
+    {"name": "C",
+      index: 0
+    },
+    {"name": "C#",
+      index: 1
+    },
+    {"name": "D",
+      index: 2
+    },
+    {"name": "D#",
+      index: 3
+    },
+    {"name": "E",
+      index: 4
+    },
+    {"name": "F",
+      index: 5
+    },
+    {"name": "F#",
+      index: 6
+    },
+    {"name": "G",
+      index: 7
+    },
+    {"name": "G#",
+      index: 8
+    },
+    {"name": "A",
+      index: 9
+    },
+    {"name": "A#",
+      index: 10
+    },
+    {"name": "H",
+      index: 11
+    }
 
-  }]);
+  ];
+  $scope.berechneZielton = function(grundton,abstand) {
+
+    var zielton=grundton.index + abstand;
+
+    $scope.zielton = zielton;
+  }
+
+}]);
+
+
