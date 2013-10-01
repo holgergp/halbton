9e62cde55478f0a69552e3f0f5548fe36c429637
holgergp <holgergp@gmail.com>
2013-10-01 22:04:36 +0200

    End to End Tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -10,36 +10,42 @@ describe('HalbtonApp', function() {
 
 
   it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser().location().url()).toBe("/view1");
+    expect(browser().location().url()).toBe("/default");
   });
 
 
-  describe('view1', function() {
+  describe('Halbtonberechnung', function() {
 
     beforeEach(function() {
       browser().navigateTo('#/view1');
     });
 
-
     it('should render view1 when user navigates to /view1', function() {
+      expect(element('[ng-view] div:first').text()).
+        toMatch(/Wenn ich von/);
+    });
+
+    it('should render "Keine Ahnung" as a result when user navigates to /view1', function() {
       expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 1/);
+        toMatch(/Keine/);
+    });
+
+    it('should render D# as a result when user navigates to /view1 and inputs basetone C and threshold 3', function() {
+      select('grundton').option('C');
+      input('abstand').enter('3');
+      expect(element('[ng-view] p:first').text()).
+        toMatch(/D#/);
+    });
+
+    it('should render "Keine Ahnung" as a result when user navigates to /view1 and enters invalid input', function() {
+      select('grundton').option('C');
+      input('abstand').enter('');
+      expect(element('[ng-view] p:first').text()).
+        toMatch(/Keine/);
     });
 
   });
 
 
-  describe('view2', function() {
 
-    beforeEach(function() {
-      browser().navigateTo('#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
