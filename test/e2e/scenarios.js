ecbffeae0e07bd1df6c61fbd089fe1d80c3084b7
holgergp <holgergp@gmail.com>
2013-10-04 00:41:47 +0200

    Test fixes

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -27,21 +27,21 @@ describe('HalbtonApp', function() {
     });
 
     it('should render "Keine Ahnung" as a result when user navigates to /view1', function() {
-      expect(element('[ng-view] p:first').text()).
+      expect(element('[ng-view] div.halbtonAnswer').text()).
         toMatch(/Keine/);
     });
 
     it('should render D# as a result when user navigates to /view1 and inputs basetone C and threshold 3', function() {
       select('grundton').option('C');
       input('abstand').enter('3');
-      expect(element('[ng-view] p:first').text()).
+      expect(element('[ng-view] div.halbtonAnswer').text()).
         toMatch(/D#/);
     });
 
     it('should render "Keine Ahnung" as a result when user navigates to /view1 and enters invalid input', function() {
       select('grundton').option('C');
       input('abstand').enter('');
-      expect(element('[ng-view] p:first').text()).
+      expect(element('[ng-view] div.halbtonAnswer').text()).
         toMatch(/Keine/);
     });
 
