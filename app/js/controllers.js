53f6afc3e2116206280dca5b7fd17c61ecdd05c0
holgergp <holgergp@gmail.com>
2013-10-03 16:05:17 +0200

    Behold a guitar ;)

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -94,57 +94,18 @@ halbtonApp.controller('HalbtonAbstandController', ['$scope', function ($scope) {
 
 
 halbtonApp.controller('GitarrenController', ['$scope', function ($scope) {
-  $scope.saiten = [
-    {"name": "C",
-      index: 0
-    }
+  $scope.normalSaiten = [
+      notee,notea,noted,noteg,noteh,notee
   ];
 
-
-  $scope.halbtoene = [
-    notec,notecsharp,noted,notedsharp,notee,notef,notefsharp,noteg,notegsharp,notea,noteasharp,noteh
-
-  ];
-  $scope.berechneZielton = function (grundton, abstand) {
-
-    if(isNullOrEmpty(grundton)||isNullOrEmpty(abstand)){
-      $scope.zielton=keinZielton;
-      return;
-    }
-
-    var halbtoene=$scope.halbtoene;
-    var zieltonIndex = mod( halbtoene.length,grundton.index + abstand );
-
-    var zielton = halbtoene[zieltonIndex];
-    $scope.zielton =isNullOrEmpty(zielton)?keinZielton:zielton;
+  $scope.bundRange = _.range(0, 12);
 
 
-  }
 
 
-  /**
-   * Überprüft (mithilfe von Underscore) on
-   * @param object
-   * Null oder undefined ist
-   * @returns {Boolean}
-   * true falls null oder undefined
-   * false falls nicht
-   */
-  var isNullOrEmpty=function(object){
-    return _.isNull(object)||_.isUndefined(object);
-  }
 
-  /**
-   * Helferfunktion die die Modulofunktion in Javascript geraderückt
-   * siehe auch http://stackoverflow.com/questions/4467539/javascript-modulo-not-behaving
-   * @param n
-   * @param m
-   * @returns {number}
-   * Beispiel n:12 m:-1 returns 11
-   */
-  var mod=function mod(n, m) {
-    return ((m % n) + n) % n;
-  }
+
+
 
 }]);
 
